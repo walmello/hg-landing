eff8acd4dfd0c7908978b7fbf252aead64935165
Walter Melo <walmello24@gmail.com>
2020-08-05 15:22:04 -0300

    logo svg

--- src/navbar.js
+++ src/navbar.js
@@ -1,7 +1,7 @@
 module.exports = 
 /*html*/`
 <nav>
-    <img src="static/img/hg.png" alt="Logotipo moderno da Hacking Growth: Letras H e G conectadas em formato triangular com preenchimento em degradê nas cores rosa, roxo e azul.">
+    <img src="static/img/hg-full.svg" alt="Logotipo moderno da Hacking Growth: Letras H e G conectadas em formato triangular com preenchimento em degradê nas cores rosa, roxo e azul.">
     <img src="static/img/menu.svg" alt="Ícone do menu com 3 linhas rosas.">
 </nav>
 `
