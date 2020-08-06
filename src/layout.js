fc3ef03d9fe8ae8dfdec9a3c530e88db7ff53d5b
Walter Melo <walmello24@gmail.com>
2020-08-06 12:20:30 -0300

    take 2

--- src/layout.js
+++ src/layout.js
@@ -22,17 +22,20 @@ module.exports = content => /*html*/`
                 font-family: 'Montserrat';
                 font-style: italic;
                 src: url('static/fonts/Montserrat/Montserrat-Italic.ttf') format("truetype");    
+                font-display: swap;
             }
 
             @font-face {
                 font-family: 'Montserrat';
                 src: url('static/fonts/Montserrat/Montserrat-Regular.ttf') format("truetype");    
+                font-display: swap;
             }
 
             @font-face {
                 font-family: 'Montserrat';
                 src: url('static/fonts/Montserrat/Montserrat-Bold.ttf');
                 font-weight: bold;
+                font-display: swap;
             }
 
             html {
