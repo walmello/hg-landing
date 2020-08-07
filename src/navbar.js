061d95e9c467a422d46ff7c0a115711e7c3f7c8f
Walter Melo <walmello24@gmail.com>
2020-08-07 14:18:12 -0300

    logo center

--- src/navbar.js
+++ src/navbar.js
@@ -10,6 +10,8 @@ module.exports =
 <nav>
     <div class="mobile">
         <div class="${nav.mobile}">
+            <div class="w-3rem"> 
+            </div>
             <img class="${nav.logo}" src="static/img/hg.png" alt="Logotipo moderno da Hacking Growth: Letras H e G conectadas em formato triangular com preenchimento em degradê nas cores rosa, roxo e azul.">
             <img id="menuOff" class="${nav.menu}" src="static/img/menu.svg" alt="Ícone do menu com 3 linhas rosas.">
             <div id="menuOn" class="none">
