296f7a7011e04412faa5f3666900a96600b508ff
walmello24 <walmello24@gmail.com>
2020-08-11 11:44:37 -0300

    fix seo form

--- src/Home/contatos.js
+++ src/Home/contatos.js
@@ -6,29 +6,39 @@ module.exports = /*html*/`
     <h2 class="${title}">Comece agora mesmo a traçar o sucesso do seu negócio</h2>
     <form class="tx-2rem p-1rem px-20LG px-20MD">
         <div class="p-1rem tx-75">
-            <label class="${label}">Nome</label><br/>
-            <input class="${input}">
-        </div>
-        <div  class="p-1rem tx-75">
-            <label class="${label}">Email</label><br/>
-            <input class="${input}">
+            <label class="${label}">
+                Nome<br/>
+                <input class="${input}">
+            </label>
         </div>
         <div class="p-1rem tx-75">
-            <label class="${label}">Nome da empresa</label><br/>
-            <input class="${input}">
+            <label class="${label}">
+                Email<br/>
+                <input class="${input}">
+            </label>
         </div>
         <div class="p-1rem tx-75">
-            <label class="${label}">Quanto investe em marketing atualmente?</label><br/>
-            <select class="${input}" name="" id="">
-                <option value="">R$ 3.000,00 a R$ 5.000,00</option>
-                <option value="">R$ 5.000,00 a R$ 10.000,00</option>
-                <option value="">R$ 10.000,00 a R$ 25.000,00</option>
-                <option value="">Mais de R$ 25.000,00</option>
-            </select>
+            <label class="${label}">
+                Nome da empresa<br/>
+                <input class="${input}">
+            </label>
         </div>
         <div class="p-1rem tx-75">
-            <label class="${label}">Mensagem</label><br/>
-            <textarea class="${input} h-4rem" style="margin-bottom: -2px"></textarea><br/>
+            <label class="${label}">
+                Quanto investe em marketing atualmente?<br/>
+                <select class="${input}" name="" id="">
+                    <option value="">R$ 3.000,00 a R$ 5.000,00</option>
+                    <option value="">R$ 5.000,00 a R$ 10.000,00</option>
+                    <option value="">R$ 10.000,00 a R$ 25.000,00</option>
+                    <option value="">Mais de R$ 25.000,00</option>
+                </select>
+            </label>            
+        </div>
+        <div class="p-1rem tx-75">
+            <label class="${label}">
+                Mensagem<br/>
+                <textarea class="${input} h-4rem" style="margin-bottom: -2px"></textarea>
+            </label>  
         </div>
         <div class="flx j-between px-1rem">
             <div></div>
--- src/Home/social.js
+++ src/Home/social.js
@@ -2,22 +2,28 @@ module.exports = /*html*/`
     <aside class="bg-dark h-100 w-5rem brr-c-primary c-white">
         <div class="mobile">
             <ul class="h-4rem m-0 bg-dark fixed w-100v p-05rem bottom flx j-around a-center ul-none brd-box brt-c-primary c-white">
-                <li>F</li>
-                <li>L</li>
-                <li>T</li>
+                <li>
+                    <a class="${link}" href="#">F</a>
+                </li>                
+                <li>
+                    <a class="${link}"  href="#">L</a>
+                </li>                
+                <li>
+                    <a class="${link}"  href="#">T</a>
+                </li>
             </ul>
         </div>
         <div class="desktop">
             <ul class="ul-none h-100 flx flx-col j-around li-rotate m-0 tx-bold">
                 <div></div>
                 <li>
-                    <a class="${link}">Linkedin</a>
+                    <a class="${link}" href="#">Linkedin</a>
                 </li>                
                 <li>
-                    <a class="${link}">Instagram</a>
+                    <a class="${link}"  href="#">Instagram</a>
                 </li>                
                 <li>
-                    <a class="${link}">Facebook</a>
+                    <a class="${link}"  href="#">Facebook</a>
                 </li>
                 <div></div>
             </ul>
